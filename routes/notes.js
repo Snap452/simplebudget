5a120dbdda9ae5ce0e7c22460d56ba7d93f86971
Luke@Butte <sathrumlu@butte.edu>
2017-10-25 20:27:13 +0000

    Adding end of lecture express application changes

---
+++ routes/notes.js
@@ -0,0 +1,38 @@
+'use strict';
+var util = require('util');
+var express = require('express');
+var router = express.Router();
+var notes = require('../models/notes-memory');
+
+// Add Note.
+router.get('/add', (req, res, next) => {
+  res.render('noteedit', {
+    title: "Add a Note",
+    docreate: true,
+    notekey: "",
+    note: undefined
+  });
+});
+module.exports = router;
+
+// Save Note
+router.post('/save', (req, res, next) => {
+  var p;
+  if (req.body.docreate === "create") {
+    p = notes.create(req.body.notekey,
+                     req.body.title,
+                     req.body.body);
+  } else {
+    p = notes.update(req.body.notekey,
+                     req.body.title,
+                     req.body.body);
+  }
+  p.then(note => {
+    res.redirect('/notes/view?key='+ req.body.notekey);
+  })
+  .catch(err => { next(err); });
+});
+
+
+
+
